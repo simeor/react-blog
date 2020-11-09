577e7a4f379726f1b3253fa0f1a0b9cd71a7cfef
simeo russo <simeo94@hotmail.com>
2020-11-09 16:52:34 +0000

    started redux cycle for fetching user from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,16 @@ export const fetchPosts = () => {
   return async (dispatch) => {
     const responce = await jsonPlaceholder.get('/posts');
 
-    dispatch({type: 'FETCH_POSTS', payload: responce})
+    dispatch({type: 'FETCH_POSTS', payload: responce.data})
   };
 };
 
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const reponce = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: reponce.data})
+  }
+};
 
 
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
+import UserHeader from './userHeader';
 
 class PostList extends React.Component{
 
@@ -8,13 +9,34 @@ class PostList extends React.Component{
     this.props.fetchPosts();
   }
 
+  renderList(){
+    return this.props.posts.map( post => {
+      return(
+        <div className="item" key={post.id}>
+          <i className="large middle align icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+              <UserHeader userId={post.userId}/>
+          </div>
+        </div>
+      );
+    })
+  }
+
   render(){
     return(
-      <div>
-         Post list comp
+      <div className="ui relaxed divided list">
+         {this.renderList()}
       </div>
       )
   }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = (state) => {
+  return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
